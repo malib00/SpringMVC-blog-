af96e06d059a1f0a0ca5fcf3d7a85bb36960187c
Oleg Karpov <oleg1karpov@gmail.com>
2023-02-28 23:31:20 +0400

    update: posts order from last to first

--- src/main/java/com/karpov/blog/controllers/MainController.java
+++ src/main/java/com/karpov/blog/controllers/MainController.java
@@ -3,6 +3,7 @@ package com.karpov.blog.controllers;
 import com.karpov.blog.models.Post;
 import com.karpov.blog.repo.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +17,7 @@ public class MainController {
 	@GetMapping("/")
 	public String home(Model model) {
 		model.addAttribute("title", "Home Page");
-		Iterable<Post> posts = postRepository.findAll();
+		Iterable<Post> posts = postRepository.findAll(Sort.by("timestamp").descending());
 		model.addAttribute("posts", posts);
 		return "home";
 	}
