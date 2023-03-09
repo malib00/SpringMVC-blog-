8a0a1ff27b0dd5311b0a9a69439df8ee79ee10b2
Oleg Karpov <oleg1karpov@gmail.com>
2023-03-10 02:48:14 +0400

    fix: users page sorted by id

--- src/main/java/com/karpov/blog/repo/UserRepository.java
+++ src/main/java/com/karpov/blog/repo/UserRepository.java
@@ -1,9 +1,11 @@
 package com.karpov.blog.repo;
 
 import com.karpov.blog.models.User;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+	Iterable<User> findAll(Sort sort);
 	User findByUsername(String username);
 	User findByEmailActivationCode(String code);
 }
