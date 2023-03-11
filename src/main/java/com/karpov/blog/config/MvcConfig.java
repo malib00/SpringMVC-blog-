b1a47f7a4546ef2702e8b6b9ede3bc611923a9fd
Oleg Karpov <oleg1karpov@gmail.com>
2023-03-12 02:57:46 +0400

    feature: RememberMe and HTTPS redirect

--- src/main/java/com/karpov/blog/config/MvcConfig.java
+++ src/main/java/com/karpov/blog/config/MvcConfig.java
@@ -1,10 +1,11 @@
 package com.karpov.blog.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,9 +15,10 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Value("${upload.path}")
 	private String uploadPath;
 
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/login").setViewName("login");
-	}
+	@Bean
+	public RestTemplate restTemplate () {
+		return new RestTemplate();
+	};
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
--- src/main/java/com/karpov/blog/config/WebSecurityConfig.java
+++ src/main/java/com/karpov/blog/config/WebSecurityConfig.java
@@ -4,15 +4,16 @@ import com.karpov.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices.RememberMeTokenAlgorithm;
 
 @Configuration
 @EnableWebSecurity
@@ -23,19 +24,36 @@ public class WebSecurityConfig {
 	private UserService userService;
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain securityFilterChain(HttpSecurity http, RememberMeServices rememberMeServices) throws Exception {
 		http
+				//redirect to HTTPS
+				.requiresChannel(channel ->
+						channel.anyRequest().requiresSecure())
+
 				.authorizeHttpRequests((requests) -> requests
 						.requestMatchers("/", "/news", "/about", "/register", "/activate/*", "/posts/img/**", "/static/**", "/img/**").permitAll()
 						.anyRequest().authenticated()
-				).userDetailsService(userService)
+				)
+				.userDetailsService(userService)
 				.formLogin((form) -> form
 						.loginPage("/login")
 						.permitAll()
 				)
+				.rememberMe((remember) -> remember
+						.rememberMeServices(rememberMeServices)
+						.key("remember-me-key")
+				)
 				.logout((logout) -> logout.permitAll());
 
 		return http.csrf().disable().build();
+	}
+
+	@Bean
+	RememberMeServices rememberMeServices(UserDetailsService userDetailsService) {
+		RememberMeTokenAlgorithm encodingAlgorithm = RememberMeTokenAlgorithm.SHA256;
+		TokenBasedRememberMeServices rememberMe = new TokenBasedRememberMeServices("remember-me-key", userDetailsService, encodingAlgorithm);
+		rememberMe.setMatchingAlgorithm(RememberMeTokenAlgorithm.MD5);
+		return rememberMe;
 	}
 
 	@Bean
