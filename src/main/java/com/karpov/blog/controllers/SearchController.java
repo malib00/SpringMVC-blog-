9de980983bf60144c5f10f1a824b953afc5572ce
Oleg Karpov <oleg1karpov@gmail.com>
2023-03-15 19:03:47 +0400

    clean code

--- src/main/java/com/karpov/blog/controllers/SearchController.java
+++ src/main/java/com/karpov/blog/controllers/SearchController.java
@@ -1,10 +1,8 @@
 package com.karpov.blog.controllers;
 
-import com.karpov.blog.models.Post;
 import com.karpov.blog.repo.PostRepository;
 import com.karpov.blog.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
