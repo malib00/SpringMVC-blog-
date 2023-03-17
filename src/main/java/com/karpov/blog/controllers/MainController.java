241b974bb70878243aa266b62693aae061529fd7
Oleg Karpov <oleg1karpov@gmail.com>
2023-03-18 00:07:47 +0400

    /myposts link in header

--- src/main/java/com/karpov/blog/controllers/MainController.java
+++ src/main/java/com/karpov/blog/controllers/MainController.java
@@ -1,10 +1,13 @@
 package com.karpov.blog.controllers;
 
 import com.karpov.blog.models.Post;
+import com.karpov.blog.models.User;
 import com.karpov.blog.repo.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,5 +27,12 @@ public class MainController {
 		Iterable<Post> posts = postRepository.findAll(Sort.by("timestamp").descending());
 		model.addAttribute("posts", posts);
 		return "home";
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("/myposts")
+	public String getMyPosts(@AuthenticationPrincipal User user,
+	                         Model model) {
+		return "redirect:/users/" + user.getId() + "/posts";
 	}
 }
