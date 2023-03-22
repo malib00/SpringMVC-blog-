088ecc3857656d65b81e0c699b256623054f7bc4
Oleg Karpov <oleg1karpov@gmail.com>
2023-03-22 17:58:16 +0400

    refactor: imageFile handling moved to ImageFileService

--- src/main/java/com/karpov/blog/controllers/PostsController.java
+++ src/main/java/com/karpov/blog/controllers/PostsController.java
@@ -3,6 +3,7 @@ package com.karpov.blog.controllers;
 import com.karpov.blog.models.Post;
 import com.karpov.blog.models.User;
 import com.karpov.blog.repo.PostRepository;
+import com.karpov.blog.service.ImageFileServisce;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -16,11 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Optional;
-import java.util.UUID;
 
 @Controller
 @RequestMapping("/posts")
@@ -29,8 +26,8 @@ public class PostsController {
 	@Autowired
 	private PostRepository postRepository;
 
-	@Value("${upload.path}")
-	private String uploadPath;
+	@Autowired
+	private ImageFileServisce imageFileServisce;
 
 	@GetMapping("/add")
 	public String addPostPage(Model model) {
@@ -47,71 +44,55 @@ public class PostsController {
 			@RequestParam("file") MultipartFile file,
 			Model model) throws IOException {
 		Post post = new Post(title, fullText, user);
-		String userPath = uploadPath + "/" + user.getId();
-		File uploadDir = new File(userPath);
-		if (!uploadDir.exists()) {
-			uploadDir.mkdir();
-		}
-		String fileName = UUID.randomUUID() + "." + file.getOriginalFilename();
-		file.transferTo(new File(userPath + "/" + fileName));
+		String path = String.valueOf(user.getId());
+		String fileName = imageFileServisce.save(file, path);
 		post.setFilename(fileName);
 		postRepository.save(post);
 		return "redirect:/";
 	}
 
-	@GetMapping("/{id}")
-	public String getPost(@PathVariable(value = "id") long postId, Model model) {
-		Optional<Post> post = postRepository.findById(postId);
-		if (post.isPresent()) {
-			Post retrievedPost = post.get();
-			model.addAttribute("title", retrievedPost.getTitle());
-			model.addAttribute("post", retrievedPost);
-			return "post-details";
-		} else {
-			return "404";
-		}
+	@GetMapping("/{post}")
+	public String getPost(@PathVariable Post post, Model model) {
+		model.addAttribute("title", post.getTitle());
+		model.addAttribute("post", post);
+		return "post-details";
 	}
 
 	@PreAuthorize("#post.author.id == principal.id" + "|| hasAnyAuthority('MODERATOR','ADMIN')")
 	@GetMapping("/{post}/edit")
 	public String editPost(@PathVariable Post post, Model model) {
-			model.addAttribute("title", "Post Edit: " + post.getTitle());
-			model.addAttribute("post", post);
-			return "post-edit";
+		model.addAttribute("title", "Post Edit: " + post.getTitle());
+		model.addAttribute("post", post);
+		return "post-edit";
 	}
 
 	@PreAuthorize("#post.author.id == principal.id" + "|| hasAnyAuthority('MODERATOR','ADMIN')")
 	@PostMapping("/{post}/edit")
 	public String postUpdate(@AuthenticationPrincipal User user,  //TODO looks like bug, who is user, who is authenticated
-	                         @PathVariable(value = "post") Post post,
+	                         @PathVariable Post post,
 	                         @RequestParam String title,
 	                         @RequestParam String fullText,
 	                         @RequestParam("file") MultipartFile file,
 	                         Model model) throws IOException {
-			post.setTitle(title);
-			post.setFullText(fullText);
-			if (!file.isEmpty()) {
-				String oldFileName = post.getFilename();
-				String userPath = uploadPath + "/" + user.getId();
-				File uploadDir = new File(userPath);
-				if (!uploadDir.exists()) {
-					uploadDir.mkdir();
-				}
-				String fileName = UUID.randomUUID() + "." + file.getOriginalFilename();
-				file.transferTo(new File(userPath + "/" + fileName));
-				post.setFilename(fileName);
-				Files.deleteIfExists(new File(userPath + "/" + oldFileName).toPath());
-			}
-			postRepository.save(post);
-			return "redirect:/";
+		post.setTitle(title);
+		post.setFullText(fullText);
+		if (!file.isEmpty()) {
+			String oldFileName = post.getFilename();
+			String path = String.valueOf(user.getId());
+			String newFileName = imageFileServisce.replace(file, path, oldFileName);
+			post.setFilename(newFileName);
+		}
+		postRepository.save(post);
+		return "redirect:/";
 	}
 
 	@PreAuthorize("#post.author.id == principal.id" + "|| hasAnyAuthority('MODERATOR','ADMIN')")
-	@PostMapping("/{id}/remove")
-	public String postDelete(@PathVariable(value = "id") Post post, Model model) throws IOException {
-			String userPath = uploadPath + "/" + post.getAuthor().getId();
-			Files.deleteIfExists(new File(userPath + "/" + post.getFilename()).toPath());
-			postRepository.delete(post);
-			return "redirect:/";
+	@PostMapping("/{post}/remove")
+	public String postDelete(@PathVariable Post post, Model model) throws IOException {
+		String path = String.valueOf(post.getAuthor().getId());
+		String fileName = post.getFilename();
+		imageFileServisce.delete(path, fileName);
+		postRepository.delete(post);
+		return "redirect:/";
 	}
 }
