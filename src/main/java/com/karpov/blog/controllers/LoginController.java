d22bde9a773bf4496eb1ab86a7b3c25ce494cf88
Oleg Karpov <oleg1karpov@gmail.com>
2023-03-22 19:53:06 +0400

    spring security: login allowed only for guests

--- src/main/java/com/karpov/blog/controllers/LoginController.java
+++ src/main/java/com/karpov/blog/controllers/LoginController.java
@@ -1,5 +1,6 @@
 package com.karpov.blog.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
+	@PreAuthorize("isAnonymous()")
 	@GetMapping("/login")
 	public String loginPage(Model model) {
 		model.addAttribute("title", "Login");
