b7888351839f6d305aa2bb6ca6c85816f47d4152
Oleg Karpov <oleg1karpov@gmail.com>
2023-03-24 12:12:30 +0400

    bug fix: authentificated users goel in loop redirects when trying to access unavailable page

--- src/main/java/com/karpov/blog/controllers/LoginController.java
+++ src/main/java/com/karpov/blog/controllers/LoginController.java
@@ -1,6 +1,5 @@
 package com.karpov.blog.controllers;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-	@PreAuthorize("isAnonymous()")
 	@GetMapping("/login")
 	public String loginPage(Model model) {
 		model.addAttribute("title", "Login");
