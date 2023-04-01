4cc5124227c5664477a6aacf9cfce65c989fb646
Oleg Karpov <oleg1karpov@gmail.com>
2023-04-01 23:19:42 +0400

    feature: empty string fields transfered as null instead of empty string object

---
+++ src/main/java/com/karpov/blog/controllers/advice/AppBindingInitializer.java
@@ -0,0 +1,18 @@
+package com.karpov.blog.controllers.advice;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+@Controller
+public class AppBindingInitializer {
+
+	//Empty string fields will be transfered as null instead of empty string object
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+	}
+}
