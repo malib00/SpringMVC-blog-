23656042264e3ebb2310c9008db4c48a5628fc62
Oleg Karpov <oleg1karpov@gmail.com>
2023-04-02 13:32:13 +0400

    feature: custom error pages

---
+++ src/main/java/com/karpov/blog/controllers/CustomErrorController.java
@@ -0,0 +1,37 @@
+package com.karpov.blog.controllers;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+	@GetMapping("/error")
+	public String errorPage(Model model, HttpServletRequest request) {
+
+		Object statusCode = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		if (statusCode == null) {
+			model.addAttribute("title", "Error");
+			return "error/error-default";
+		} else {
+			if (Integer.valueOf(statusCode.toString()) == HttpStatus.NOT_FOUND.value()) {
+				return "error/error-not-found";
+			}
+			if (Integer.valueOf(statusCode.toString()) == HttpStatus.FORBIDDEN.value()) {
+				return "error/error-access-denied";
+			}
+			if (Integer.valueOf(statusCode.toString()) == HttpStatus.METHOD_NOT_ALLOWED.value()) {
+				return "error/error-access-denied";
+			}
+			if (Integer.valueOf(statusCode.toString()) == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+				return "error/error-internal-server-error";
+			}
+		}
+		return "error/error-default";
+	}
+}
